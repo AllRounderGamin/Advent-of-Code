f5fd4bd13269d8911b984aee05824ca3cebca405
AllRounderGamin <78315461+AllRounderGamin@users.noreply.github.com>
2023-12-17 16:57:43 +0000

    Day 4 Part 2

--- 2023/Advent of Code Day 4/Source.cpp
+++ 2023/Advent of Code Day 4/Source.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>    // std::find
+#include <unordered_map>
 
 void cardSplit(std::vector<int>& vec, std::string string) {
 	std::string input;
@@ -43,6 +44,62 @@ void problem1() {
 	std::cout << "Part 1 Result: " << result << "\n";
 }
 
+// have a map that will store card id's and how many times they need to be repeated, can get id from find : - 1
+
+void calculateDupe(std::unordered_map<int, int>& playMap, int score, int ID) {
+	for (int i = 1; i < score+1; i++) {
+		if (ID + i > 216) {
+			return;
+		}
+		else {
+			if (playMap.find(ID+i) == playMap.end()) {
+				// 1 original + the added dupe
+				playMap[ID + i] = 2;
+			}
+			else {
+				playMap[ID + i] += 1;
+			}
+		}
+	}
+}
+
+
+void problem2() {
+	std::ifstream codeFile("input.txt");
+	std::string card;
+	int cardScore, plays, result = 0;
+	std::vector<int> cardNums, winningNums;
+	std::unordered_map<int, int> playValues;
+	while (std::getline(codeFile, card)) {
+		int cardID = stoi(card.substr(card.find(" "), (card.find(":") - card.find(" "))));
+		if (playValues.find(cardID) == playValues.end()) {
+			playValues[cardID] = 1;
+		}
+		plays = 0;
+		cardNums.clear();
+		winningNums.clear();
+		card = card.substr(card.find(":") + 2, std::string::npos);
+		cardSplit(cardNums, card.substr(0, card.find("|")));
+		cardSplit(winningNums, card.substr(card.find("|") + 2));
+		cardScore = 0;
+		for (int num : winningNums) {
+			if (std::find(cardNums.begin(), cardNums.end(), num) != cardNums.end()) {
+				cardScore++;
+			}
+		}
+		do {
+			calculateDupe(playValues, cardScore, cardID);
+			plays++;
+		} while (plays < playValues[cardID]);
+	}
+	for (auto const& x : playValues) {
+		result += x.second;
+	}
+	std::cout << "Part 2 Result: " << result << "\n";
+}
+
+
 int main() {
 	problem1();
+	problem2();
 }
